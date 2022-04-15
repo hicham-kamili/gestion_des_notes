5869f6b7e64463ef846d4faf4b89e4f83828cf61
PC <PC@DESKTOP-HV29B0J>
2022-04-15 14:52:05 +0000

    constrecteur d'etudiant

--- gestion_des_notes/src/gestion_des_notes/Etudiant.java
+++ gestion_des_notes/src/gestion_des_notes/Etudiant.java
@@ -4,6 +4,12 @@ public class Etudiant {
 	private int id ;
 	private String nom ;
 	private double note ;
+	Etudiant(int i_d, String n_om, double n_ote){
+		this.id = i_d;
+		this.nom = n_om;
+		this.note = 10;
+		
+	}
 
 
 }
